1185452286f8f0d061723857b4521093feac5380
Alex Cruz <aacruz@sbcglobal.net>
2018-07-02 20:37:54 -0700

    finished day1 in class work

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// DECLARE A VARIABLE
+  var firstName;
+
+// DECLARE YOUR VARIABLE
+  firstName="Alex"; //String
+
+//DEFINE AND DECLARE YOUR VARIABLE
+  var lastName= "Cruz";
+
+  console.log(firstName, lastName);
+
+//NEW SCOPE
+  var myName;
