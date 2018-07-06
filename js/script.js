943a0193c3db4a897ce0593bf7e083524f982270
Ban-sama <34254308+Ban-sama@users.noreply.github.com>
2018-07-05 19:03:13 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// DECLARE A VARIABLE
+  var firstName;
+
+// DECLARE YOUR VARIABLE
+  firstName="Alex"; //String
+
+//DEFINE AND DECLARE YOUR VARIABLE
+  var lastName= "Cruz";
+
+  console.log(firstName, lastName);
+
+//NEW SCOPE
+  var myName;
+
+//TAKE HOME ASSIGNMENT
+  var whatIHopeToLearn="How to setup my html or website with javascript";
+  var myHobby="Video Games";
+
+  console.log(whatIHopeToLearn);
+
+  console.log(myHobby);
+
+//ATOM ERROR
